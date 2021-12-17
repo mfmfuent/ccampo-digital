2d7b3c76b8c7d167e676f269880507b9c317cb46
mfmfuent <mfmfuent@gmail.com>
2021-12-17 16:36:24 -0300

    se modifica todoel login con plantilla

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module CcampoDigital
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.enabled = false
   end
 end
