064b54d74467156ee9d05083cac9f09a2223d065
mfmfuent <mfmfuent@gmail.com>
2021-12-21 14:45:30 -0300

    se modifica application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module CcampoDigital
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.assets.enabled = false
+
+    config.assets.initialize_on_precompile = false
   end
 end
