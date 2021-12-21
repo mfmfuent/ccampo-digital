76ca0264896a6b55d5301bc960becac0f5e2988a
mfmfuent <mfmfuent@gmail.com>
2021-12-21 14:58:01 -0300

    se compila assets

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module CcampoDigital
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.assets.enabled = false
-
-    config.assets.initialize_on_precompile = false
+  
   end
 end
